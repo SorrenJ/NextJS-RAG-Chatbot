4dc66bc666938dae6c9089fb1bd63f2aea211364
SorrenJ <soalexjao@gmail.com>
2025-10-07 16:32:55 -0700

    process env variables, place url to scrape, make strict mode false

--- my-next-app/scripts/loadDb.ts
+++ my-next-app/scripts/loadDb.ts
@@ -8,3 +8,21 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 
 import "dotenv/config"
 
+const {ASTRA_DB_NAMESPACE, 
+    ASTRA_DB_COLLECTION, 
+    ASTRA_DB_API_ENDPOINT, 
+    ASTRA_DB_API_APPLICATION_TOKEN, 
+    OPENAI_API_KEY  
+} = process.env
+
+
+const openai = new OpenAI ({ apiKey: OPENAI_API_KEY })
+
+// place links for more web scraping
+const f1Data = [
+    'https://en.wikipedia.org/wiki/Formula_One'
+]
+
+const client = new DataAPIClient(ASTRA_DB_API_APPLICATION_TOKEN)
+    
+   const db = client.db( ASTRA_DB_API_ENDPOINT, { namespace: ASTRA_DB_NAMESPACE});
